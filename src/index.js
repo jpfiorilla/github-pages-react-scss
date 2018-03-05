846b63828635b20854727e70ce38649dbc53560c
John Fiorilla <john.fiorilla@loan-street.com>
2018-03-05 12:37:50 -0500

    actions, reducers init

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './components/App';
+import reducers from './redux/reducers';
+
+const store = createStore(reducers);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root'),
 );
---
+++ src/redux/actions/actions.js
@@ -0,0 +1,10 @@
+export const SAMPLE_ACTION = 'SAMPLE_ACTION';
+
+export function sampleAction(sampleProp) {
+  return {
+    type: SAMPLE_ACTION,
+    payload: {
+      sampleProp,
+    },
+  };
+}
